c2a9f26781c8ed9f7355230083c96b408cf1f2ae
bhb <selre1@naver.com>
2023-07-02 21:34:34 +0900

    init

---
+++ src/test/java/com/bhb/book/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhb.book.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
